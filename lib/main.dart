b62f402d113d324ca069c637f637fde6ff2516b4
Kiran Johns <kiranjohns369@gmail.com>
2020-04-17 13:34:26 +0530

    DiceMwon

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,70 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(MyApp());
+void main() => runApp(DiceApp());
 
+class DiceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red[700],
+        appBar: AppBar(backgroundColor: Colors.red[700],title: Text('DiceMwon'),elevation: 0,),
+        body: DiceHome(),
+      ),
+    );
+  }
+}
+
+class DiceHome extends StatefulWidget {
+  @override
+  _DiceHomeState createState() => _DiceHomeState();
+}
+
+class _DiceHomeState extends State<DiceHome> {
+
+  int right = 1;
+  int left = 1;
+
+  void changeDice(){
+    right = Random().nextInt(6) + 1;
+    left = Random().nextInt(6) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Row(children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset('assets/dice$left.png'),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset('assets/dice$right.png'),
+              ),)
+          ],),
+          RaisedButton(
+            onPressed: (){
+              setState(() {
+                changeDice();
+              });
+            },
+            textColor: Colors.red[800],
+            child: Text('Roll'),
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
